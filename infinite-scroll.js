d85780aa8056f4d9e731c81c9c1efb494e041aed
Ryan Zec <code@ryanzec.com>
2013-10-02 19:48:18 -0400

    Inline documentation updates

--- infinite-scroll.js
+++ infinite-scroll.js
@@ -1,16 +1,30 @@
 /**
- * Enables infinite scrolling
+ * # Infinite Scroll
+ *
+ * This directive enables infinite scrolling.
+ *
+ * ```html
+ * <script type="text/javascript">
+ * //some where in a controller
+ * $scope.infiniteScroll = function() {
+ *   //add some more visible data
+ * }
+ * ```
+ *
+ * @todo: need to test and think about performance will large data sets
  *
  * @module nag.infiniteScroll
  * @ngdirective nagInfiniteScroll
+ *
+ * @nghtmlattribute {function} nag-infinite-scroll Function to call when the element in near the bottom of scrolling
  */
 angular.module('nag.infiniteScroll', [])
 .directive('nagInfiniteScroll', [function() {
-  return function(scope, elm, attr) {
-    var raw = elm[0];
+  return function(scope, element, attr) {
+    var rawElement = element[0];
     
-    elm.bind('scroll', function() {
-      if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+    element.bind('scroll', function() {
+      if (rawElement.scrollTop + rawElement.offsetHeight >= rawElement.scrollHeight) {
         scope.$apply(attr.nagInfiniteScroll);
       }
     });
